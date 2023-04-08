5ed40619ed1ab56fb63266b49e71fb61a688feca
Sunandita-R <118152848+Sunandita-R@users.noreply.github.com>
2023-04-08 10:30:53 +0530

    Add files via upload

--- files.js
+++ files.js
@@ -1,29 +1,27 @@
-
 $(function() {
-    //cache the window object
-    var $window = $(window);
-  
-    //parallax background effect
-    $('section[data-type="background"]').each(function(){
-  
-      var $bgobj = $(this); //assigning the object
-  
-      $(window).scroll(function(){
-  
-        //scroll the bg at var speed
-        //the yPos is negative because we are scrolling it up
-  
-        var yPos = -($window.scrollTop() / $bgobj.data('speed'));
-  
-        //put together our final bg position
-        var coords = '50% '+ yPos + 'px';
-  
-        //move the bg
-        $bgobj.css({ backgroundPosition: coords });
-  
-      }); //end window scroll
-  
-    });
+  //cache the window object
+  var $window = $(window);
+
+  //parallax background effect
+  $('section[data-type="background"]').each(function(){
+
+    var $bgobj = $(this); //assigning the object
+
+    $(window).scroll(function(){
+
+      //scroll the bg at var speed
+      //the yPos is negative because we are scrolling it up
+
+      var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+
+      //put together our final bg position
+      var coords = '50% '+ yPos + 'px';
+
+      //move the bg
+      $bgobj.css({ backgroundPosition: coords });
+
+    }); //end window scroll
+
   });
-  
-  
+ 
+});
